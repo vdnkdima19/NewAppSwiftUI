96290e3130e4f378bb1191e0d3a0dd7fd7a09bcb
vdnk_d <dmitryvdvnk@gmail.com>
2024-06-23 13:58:53 +0300

    Initial Commit

---
+++ NewAppSwiftUI/NewAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewAppSwiftUIApp.swift
+//  NewAppSwiftUI
+//
+//  Created by Dmitry Vdovenko on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
